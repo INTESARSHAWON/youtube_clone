4ef44bd9ffb2e50ffdc18a80372c240f11f66be1
Intesar Shawon <intesarshawon@gmail.com>
2024-11-08 20:34:36 +0600

    calling api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
-import { Box, Stack, Typography } from '@mui/material'
-import { useState, useEffect} from 'react'
-import Sidebar from './Sidebar'
+import { Box, Stack, Typography } from '@mui/material';
+import { useState, useEffect} from 'react';
+import { Sidebar, Videos }from './';
 
 const Feed = () => {
   return (
@@ -11,8 +11,20 @@ const Feed = () => {
           <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
             Copyright 2024 Youtube Clone
           </Typography>
-
       </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white'}}>
+           
+          New <span style={{
+              color: '#F31503',
+          }}>
+               Videos
+          </span>
+        </Typography>
+        <Videos Videos={[]}/>
+      </Box>
+
     </Stack>
   )
 }
